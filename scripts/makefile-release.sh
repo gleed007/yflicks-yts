b5a764f1601eeac73118c47b04be7bbd42eb5f6a
atifcppprogrammer <54069350+atifcppprogrammer@users.noreply.github.com>
2024-01-16 05:43:18 +0500

    chore: updated release script to create tag and generate changelog

--- scripts/makefile-release.sh
+++ scripts/makefile-release.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-release_name="yflicks-yts-$1"
+release_dir="dist"
+release_version=$1
+release_name="yflicks-yts-$release_version"
+release_path="$release_dir/$release_name"
+
+git-chglog --next-tag $release_version --output CHANGELOG.md 
+git add CHANGELOG.md
+git commit -m "chore(release): v$release_version"
+git tag -sa -m "yflicks-yts-$release_version" v$release_version 
+
+rm -rf $release_dir
 source_files=($(ls --ignore={.git,.gitignore,Makefile,githooks,scripts}))
-mkdir $release_name
+mkdir -p $release_path
+
 for source_file in ${source_files[@]}; do 
-  cp -r $source_file $release_name
+  cp -r $source_file $release_path
 done
 
-echo $release_name.tar.gz
+cd $release_dir && echo $release_name.tar.gz
 tar -czvf $release_name.tar.gz $release_name
 
 echo && echo $release_name.zip
