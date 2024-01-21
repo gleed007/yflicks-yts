3fa2f5d5f365e4b4849c905b5a3b7c8cc7219d01
atifcppprogrammer <54069350+atifcppprogrammer@users.noreply.github.com>
2024-01-21 19:56:29 +0500

    feat(client): updated methods to require `context.Context` argument

--- yts.go
+++ yts.go
@@ -1,27 +1,31 @@
 package yts
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"net/url"
 
-	"github.com/atifcppprogrammer/yflicks-yts/internal/netutils"
 	"github.com/atifcppprogrammer/yflicks-yts/internal/validate"
 )
 
 const APIBaseURL = "https://yts.mx/api/v2"
 
 type Client struct {
-	apiBaseURL string
+	apiBaseURL    string
+	apiHTTPClient *http.Client
 }
 
 type FilterValidationError validate.StructValidationError
 
 func NewClient() *Client {
-	return &Client{APIBaseURL}
+	return &Client{APIBaseURL, &http.Client{}}
 }
 
-func (c Client) SearchMovies(filters *SearchMoviesFilters) (*SearchMoviesResponse, error) {
+func (c Client) SearchMovies(ctx context.Context, filters *SearchMoviesFilters) (*SearchMoviesResponse, error) {
 	queryString, err := filters.getQueryString()
 	if err != nil {
 		return nil, err
@@ -29,7 +33,7 @@ func (c Client) SearchMovies(filters *SearchMoviesFilters) (*SearchMoviesRespons
 
 	parsedPayload := &SearchMoviesResponse{}
 	targetURL := c.getEndpointURL("list_movies.json", queryString)
-	err = netutils.GetPayload(targetURL, parsedPayload)
+	err = c.getPayload(ctx, targetURL, parsedPayload)
 	if err != nil {
 		return nil, err
 	}
@@ -37,7 +41,7 @@ func (c Client) SearchMovies(filters *SearchMoviesFilters) (*SearchMoviesRespons
 	return parsedPayload, nil
 }
 
-func (c Client) GetMovieDetails(filters *MovieDetailsFilters) (*MovieDetailsResponse, error) {
+func (c Client) GetMovieDetails(ctx context.Context, filters *MovieDetailsFilters) (*MovieDetailsResponse, error) {
 	queryString, err := filters.getQueryString()
 	if err != nil {
 		return nil, err
@@ -45,7 +49,7 @@ func (c Client) GetMovieDetails(filters *MovieDetailsFilters) (*MovieDetailsResp
 
 	parsedPayload := &MovieDetailsResponse{}
 	targetURL := c.getEndpointURL("movie_details.json", queryString)
-	err = netutils.GetPayload(targetURL, parsedPayload)
+	err = c.getPayload(ctx, targetURL, parsedPayload)
 	if err != nil {
 		return nil, err
 	}
@@ -53,7 +57,7 @@ func (c Client) GetMovieDetails(filters *MovieDetailsFilters) (*MovieDetailsResp
 	return parsedPayload, nil
 }
 
-func (c Client) GetMovieSuggestions(movieID int) (*MovieSuggestionsResponse, error) {
+func (c Client) GetMovieSuggestions(ctx context.Context, movieID int) (*MovieSuggestionsResponse, error) {
 	if movieID <= 0 {
 		return nil, errors.New("provided movieID must be at least 1")
 	}
@@ -66,12 +70,43 @@ func (c Client) GetMovieSuggestions(movieID int) (*MovieSuggestionsResponse, err
 
 	parsedPayload := &MovieSuggestionsResponse{}
 	targetURL := c.getEndpointURL("movie_suggestions.json", queryString)
-	err := netutils.GetPayload(targetURL, parsedPayload)
+	err := c.getPayload(ctx, targetURL, parsedPayload)
 	if err != nil {
 		return nil, err
 	}
 
 	return parsedPayload, nil
+}
+
+func (c Client) getPayload(ctx context.Context, targetURL string, payload interface{}) error {
+	parsedURL, err := url.Parse(targetURL)
+	if err != nil {
+		return err
+	}
+
+	parsed := parsedURL.String()
+	request, err := http.NewRequestWithContext(ctx, "GET", parsed, http.NoBody)
+	if err != nil {
+		return err
+	}
+
+	response, err := c.apiHTTPClient.Do(request)
+	if err != nil {
+		return err
+	}
+
+	defer response.Body.Close()
+	rawPayload, err := io.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(rawPayload, payload)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (c Client) getEndpointURL(path, query string) string {
